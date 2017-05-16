a2a33c14142cc238bd098a098b3c1d591bab3d24
Meltem <melltemkilic@gmail.com>
2017-05-15 21:07:43 -0400

    the function for order confirmation email has been implemented here. It passed to the orders controller to get invoked in case of the success of an order

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def orderconf_email(user)
-    @user = user
-    @url = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to Jungle')
+  def orderconf_email(order)
+    @order = order
+    @user = @order.user
+    @url = "http://0.0.0.0:3000/order/#{order.id}"
+    mail(to: @user.email, subject: "Your order has been confirmed (order number: #{order.id})")
   end
 end
