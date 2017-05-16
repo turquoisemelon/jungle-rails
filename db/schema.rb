1a64050cc3d235093dfa733fb1f2e714a620dab8
Meltem <melltemkilic@gmail.com>
2017-05-15 21:09:50 -0400

    user_id field added to the orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515160700) do
+ActiveRecord::Schema.define(version: 20170516003605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170515160700) do
     t.datetime "updated_at",       null: false
     t.string   "stripe_charge_id"
     t.string   "email"
+    t.integer  "user_id"
   end
 
   create_table "products", force: :cascade do |t|
