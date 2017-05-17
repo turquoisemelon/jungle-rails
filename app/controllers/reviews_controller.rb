65a0750f2ceee23f6aebec047a319c120e39951f
Meltem <melltemkilic@gmail.com>
2017-05-16 20:57:05 -0400

    logic for creating review is in progress. There are bugs to fix

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,15 @@
 class ReviewsController < ApplicationController
   def create
-    raise "yay, I'm here!"
+    puts params.inspect
+    @product = Product.find(params[:product_id])
+    @review = Review.new #pass params coming from user
+    @review.user = current_user
+    @review.save
+
+    if (@product.reviews)
+      redirect_to product_reviews_path(params[:product_id])
+    else
+      redirect_to product_path(params[:product_id])
+    end
   end
 end
